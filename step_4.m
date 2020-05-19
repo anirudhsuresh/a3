124a4e828e9de8f63b4f75883c4fd3be3190cafb
anirudhsuresh <56661161+anirudhsuresh@users.noreply.github.com>
2020-05-18 17:27:28 -0700

    updated step 4

--- step_4.m
+++ step_4.m
@@ -1,23 +1,27 @@
 route=[1,13,3,6,16,19,9,10,20,17,14,4,7,11,21,18,8,5,15,12,2,22];
 
 global XY
-s=route(2:21)
+s=route(2:21);
+for i=1:21
+    co(i,1)=XY(i,1)';
+    co(i,2)=XY(i,2)';
+end
 N=10;
 R=3;
 W=2.5;
-pts=20;
+pts=5;
 j=0;
 direction=0;
-for i=1:19
+n=length(s);
+for i=1:n-1
     p=s(i);
     q=s(i+1);
-        start_p=XY(p,:);
-    %     end_p=XY(q,:);
-    %     start_p
-    %     end_p
-    if abs(s(i)-s(i+1))==N      % its a straight line
-        start_p=XY(i,:);
-        end_p=XY(i+1,:);
+    start_p=XY(p,:);
+    end_p=XY(q,:);
+    
+    if abs(p-q)==N      % its a straight line
+%         start_p=XY(p,:);
+%         end_p=XY(q,:);
         path=Line_way_p_gen(start_p,end_p,pts);
     else                        % its a headland manuver
         % to identify the direction of the current turn
@@ -39,9 +43,9 @@ for i=1:19
         d=abs(s(i)-s(i+1));
         dist_n=d*W;
         
-        if dist_n>=2*R             % if f is greater than the min then pi turns
+        if dist_n>=2*R      % if f is greater than the min then pi turns
             path= pi_turn(R, dist_n,start_p, direction);
-        else  %             else omega turn
+        else                % else omega turn
             path= omgea_turn(R, dist_n,start_p, direction);
         end
     end
@@ -51,7 +55,12 @@ for i=1:19
         D(j+k,2)=path(k,2);
     end
     j=j+k;
+ 
 end
-D
- plot(D(:,1),D(:,2));pause(1)
+path_exp=D;
+% path_m=manhattan_path(XY(s(1),:),XY(s(2),:));
+% start_path=path_m;
+% path_m=manhattan_path(XY(s(20),s(21)));
+% end_path=path_m;
+ plot(path_exp(:,1),path_exp(:,2))
 
